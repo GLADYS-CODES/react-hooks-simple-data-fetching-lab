f4a19eb752b4dafee495f6629f941b05ee543e94
GLADYS <mungaiglado106@gmail.com>
2022-07-16 12:47:49 +0300

    intial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
+const { useState, useEffect } = require("react");
+
 // create your App component here
+function App() {
+    const [dogPic, setDogPic] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then((result) => {
+            setDogPic(result.message);
+
+    });
+}, []);
+
+
+if (!dogPic) return <p>Loading...</p>;
+
+return<img src={dogPic} alt="A Random Dog" />;
+
+
+}
+
+
+export default App;
